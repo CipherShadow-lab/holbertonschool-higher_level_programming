695e32e98e8adfb70108a7dc8a73414ca1754f34
CipherShadow <ciphershadow1@gmail.com>
2025-09-03 05:44:41 +1000

    Fixed length of print lines

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,23 @@
 import random
 number = random.randint(-10000, 10000)
 
+
 def last_digit(number):
     if number < 0:
         return -(abs(number) % 10)
     else:
         return number % 10
+    
 
 if last_digit(number) > 5:
-    print(f"Last digit of {number} is {last_digit(number)} and is greater than 5")
+    print(
+        f"Last digit of {number} is {last_digit(number)} "
+        "and is greater than 5"
+    )
 elif last_digit(number) == 0:
     print(f"Last digit of {number} is {last_digit(number)} and is 0")
 elif last_digit(number) < 6 and last_digit(number) != 0:
-    print(f"Last digit of {number} is {last_digit(number)} and is less than 6 and not 0")
+    print(
+        f"Last digit of {number} is {last_digit(number)} "
+        "and is less than 6 and not 0"
+    )
