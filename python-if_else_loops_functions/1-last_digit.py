c0ed6c71838f8bad491ca3d8199124eeee52969c
CipherShadow <ciphershadow1@gmail.com>
2025-09-03 05:34:16 +1000

    added function to handle neg numbers in last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit} ", end="")
 
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
-elif last_digit < 6 and last_digit != 0:
-    print("and is less than 6 and not 0")
+def last_digit(number):
+    if number < 0:
+        return -(abs(number) % 10)
+    else:
+        return number % 10
+
+if last_digit(number) > 5:
+    print(f"Last digit of {number} is {last_digit(number)} and is greater than 5")
+elif last_digit(number) == 0:
+    print(f"Last digit of {number} is {last_digit(number)} and is 0")
+elif last_digit(number) < 6 and last_digit(number) != 0:
+    print(f"Last digit of {number} is {last_digit(number)} and is less than 6 and not 0")
