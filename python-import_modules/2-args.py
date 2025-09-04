d68e74b4d1e23670f3f44e78450c5e9132a95d52
cipher <ciphershadow1@gmail.com>
2025-09-04 13:39:59 +1000

    "Added execution permissions and task 2 file"

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    args = sys.argv[1:] # excludes progam/script name
+    count = len(args)
+
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+
+    for i, arg in enumerate(args, start=1): 
+        print("{}: {}".format(i, arg))
