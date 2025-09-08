13762ce0f040f461f4b009b2c83a2c298cdc15d8
cipher <ciphershadow1@gmail.com>
2025-09-09 06:18:50 +1000

    "Add 8-multiple_returns task"

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    result = ""
+    i = 0
+
+    if len(sentence) == 0:
+            return (None)
+    else:
+        result += sentence[0]
+        return (len(sentence), result)
