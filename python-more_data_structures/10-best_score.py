ced12a050992081b2f555078838404f4cb9e0524
cipher <ciphershadow1@gmail.com>
2025-09-09 14:54:21 +1000

    "Added comment"

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary:  # checks is dictionary is None or empty
         return (None)
     highest_score = max(a_dictionary, key=a_dictionary.get)
     return (highest_score)
