88ea164fb2a2bccc5f457f255e84c7f7d8eab012
cipher <ciphershadow1@gmail.com>
2025-09-10 13:13:13 +1000

    "Add 0-add_integer code and test file"

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers or floats (first cast to int).
+
+    Args:
+        a: The first number (int or float).
+        b: The second number (int or float), default is 98.
+
+    Returns:
+        The sum of a and b as an integer.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+
+#!/usr/bin/python3
+add_integer = __import__('0-add_integer').add_integer
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
