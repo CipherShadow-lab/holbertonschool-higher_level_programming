9f10ff4092c2b0801b08d285f691a49db36a4243
CipherShadow <ciphershadow1@gmail.com>
2025-09-12 08:43:23 +1000

    "Fixed 5-text_indendation code"

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """
-This module defines the text_indentation function
+Defines a text-indentation function.
 """
 
 
 def text_indentation(text):
-    """ Function that prints indented text.
+    """Print text with two new lines after each '.', '?', and ':'.
+
     Args:
-        text (str): text to print
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    length = len(text)
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    while i < length:
-        print(text[i], end="")
-
-        if text[i] in ['.', '?', ':']:
-            print("\n")
-
-            i += 1
-            while i < length and text[i] == " ":
-                i += 1
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
             continue
-
-        i += 1
+        c += 1
