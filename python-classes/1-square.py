e47449624c29947608128b79895ee7408ab06387
CipherShadow <ciphershadow1@gmail.com>
2025-09-15 18:54:35 +1000

    "Added tasks 0-3square.py"

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square
+"""
+
+
+class Square:
+    """
+    This class defines a square by its size.
+    """
+    
+    def __init__(self, size):
+        """
+        Initialises the square with a specific size.
+        The size is a private attribute.
+        """
+        self.__size = size
---
+++ python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square
+"""
+
+
+class Square:
+    """
+    This class defines a square by its size.
+    """
+    
+    def __init__(self, size=0):
+        """
+        Initialises the square with a specific size.
+        The size is a private attribute.
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of a square.
+        Returns:
+            int: The area of a square.
+        """
+        return self.__size ** 2
