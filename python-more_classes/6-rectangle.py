1dc1b222f25b4974f16fc422950431c8ab6275cb
CipherShadow <ciphershadow1@gmail.com>
2025-09-17 06:11:51 +1000

    "Fixed attribute error"

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
         """Initialisation of Instance"""
         self.width = width
         self.height = height
+        self.number_of_instances = number_of_instances
         Rectangle.number_of_instances += 1
 
     @property
