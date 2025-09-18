dc9b1700ceaa3e6fb1c4772aa76c0bdec9027896
CipherShadow <ciphershadow1@gmail.com>
2025-09-18 16:40:03 +1000

    "Add 100-singly_linked_list"

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """Getter for private data attribute"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Setter for data"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+    
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        """
+        Special method to represent the object as a string
+        and make the print() work
+        """
+
+
+        node = self.__head
+        values = []
+
+        while node is not None:
+            values.append(str(node.data))
+            node = node.next_node
+        
+
+        return "\n".join(values)
+    
+
+
+    def sorted_insert(self, value):
+        """Method inserts a new Node into a singly linked list"""
+        new_node = Node(value)
+
+        if self.__head is None:
+            self.__head = new_node
+            return
+
+        if value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        current = self.__head
+        while (
+            current.next_node is not None and
+            current.next_node.data < value
+            ):
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
