887d8a800e8b04cd8b9f0d201c832b4016f93c4d
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 16:53:34 +1000

    "Removed import module line"

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,8 +5,18 @@ from BaseGeometry.
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry():
+    """Defined class called BaseGeometry"""
+    def area(self):
+        """Method raises Exception of area not implemented."""
+        raise Exception("area() is not implemented")
 
+    def integer_validator(self, name, value):
+        """Method validates that the value is an integer"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """Defines the Rectangle class that inherets from BaseGeometry"""
