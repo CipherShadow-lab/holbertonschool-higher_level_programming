a281dbe1df6ca9b5402841d56cc81999e838685a
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 16:52:14 +1000

    "Add 8-rectangle.py"

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Rectangle Module.
+Contains a class Rectangle that inherits
+from BaseGeometry.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines the Rectangle class that inherets from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
