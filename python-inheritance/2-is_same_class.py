98dbd56bf1b7f3f97320bc83fb3d1ee36c7224bd
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 15:18:18 +1000

    "Added extra blank line - line 8"

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,7 @@ exactly an instance of the specified class.
 Otherwise returns False.
 """
 
+
 def is_same_class(obj, a_class):
     """Checks if an object is exaclty an instance of a class."""
     return type(obj) is a_class
