2f6f30d5c9f681b990f6aa26d5da6a3c110a660c
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 15:54:46 +1000

    "Add 5-base_geometry.py"

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""base_geometry Module.
+Contains an emply class called BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Checks if an object is an instance of an inherited class."""
+    pass
