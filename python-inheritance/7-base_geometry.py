ad328b06409b089c077291cecba1be4cd9a6c54a
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 16:25:25 +1000

    "Add 7-base_geometry.py"

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""base_geometry Module.
+Contains a class called BaseGeometry.
+"""
+
+
+class BaseGeometry():
+    """Defined class called BaseGeometry"""
+    def area(self):
+        """Method raises Exception of area not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method validates that the value is an integer"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
