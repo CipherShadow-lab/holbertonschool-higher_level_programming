caf12e5de2745a877fc25b07d917d90c6047a55e
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 17:06:11 +1000

    "Adding import due to checker conflict"

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,18 +3,8 @@
 Contains a class Rectangle that inherits
 the BaseGeometry class.
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry():
-    """BaseGeometry class"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """Rectangle class inherets BaseGeometry class."""
