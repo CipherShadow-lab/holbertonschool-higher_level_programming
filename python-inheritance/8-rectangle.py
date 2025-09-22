2e28f9b5631fe38b5d5e3cd44d209d413aec1226
CipherShadow <ciphershadow1@gmail.com>
2025-09-22 17:03:57 +1000

    "Remove any conflicting comments for checker"

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,20 +6,18 @@ the BaseGeometry class.
 
 
 class BaseGeometry():
-    """Defined class called BaseGeometry"""
+    """BaseGeometry class"""
     def area(self):
-        """Method raises Exception of area not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method validates that the value is an integer"""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """Defines the Rectangle class that inherets BaseGeometry class."""
+    """Rectangle class inherets BaseGeometry class."""
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
