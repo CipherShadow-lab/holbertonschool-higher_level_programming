aee14b982632347ae8c967f87a7efb3150e517ae
CipherShadow <ciphershadow1@gmail.com>
2025-09-24 07:43:52 +1000

    Add task_01_duck_typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    """Defines a Shape Abstract Class."""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = abs(radius)
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width * self.height)
+
+def shape_info(shape):
+    """Print area and perimeter of shape."""
+    print (f"Area: {shape.area()}")
+    print (f"Perimeter: {shape.perimeter()}")
+
+if __name__ == "__main__":
+    "Functionality testing."
+    circle = Circle(radius=8)
+    rectangle = Rectangle(width=4, height=5)
+
+    shape_info(circle)
+    shape_info(rectangle)
