1df301a9542fbff80bad9fe87650431acfc2466b
CipherShadow <ciphershadow1@gmail.com>
2025-09-24 08:09:51 +1000

    Add task_03_countediterator

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class CountedIterator():
+    """An interator that tracks number of items iterated over."""
+
+    def __init__(self, iterable):
+        self.iterable = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """Return next item and increment counter"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        """Return count number of items iterated."""
+        return self.count
+
+    def __iter__(self):
+        "Return iterator."
+        return self
