e3cc831fe8aafb3c694547f64a4b87f38c5b5333
cipher <ciphershadow1@gmail.com>
2025-09-29 15:36:53 +1000

    Add additional comments

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -9,4 +9,4 @@ import json
 def to_json_string(my_obj):
     """function returns JSON representation of an object"""
 
-    return json.dumps(my_obj)
+    return json.dumps(my_obj)  # converts obj into json-formatted string
