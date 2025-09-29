49a1ffc49013bf43a9c66fe33d54f3b0d639d43a
cipher <ciphershadow1@gmail.com>
2025-09-29 15:05:17 +1000

    Fixed encoding statement

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 def read_file(filename=""):
     """function reads and prints a utf-8 text file"""
 
-    with open(filename, "r", encode="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         content = filename.read()
         print(content, end="")
