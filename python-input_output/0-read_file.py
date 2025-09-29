5749b33553a843ebc33ec69ca6884b804c6f6979
cipher <ciphershadow1@gmail.com>
2025-09-29 15:08:09 +1000

    Add extra blank line - line 4

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that reads a text file and prints to stdout"""
 
+
 def read_file(filename=""):
     """function reads, prints and returns a utf-8 text file"""
 
