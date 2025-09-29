02ed2ba722768270fca1ebd311372c8fc277c125
cipher <ciphershadow1@gmail.com>
2025-09-29 15:00:59 +1000

    Add comments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """function reads a text file and prints to stdout"""
+
     with open(filename, "r", encode="utf-8") as f:
         content = filename.read()
         print(content, end="")
