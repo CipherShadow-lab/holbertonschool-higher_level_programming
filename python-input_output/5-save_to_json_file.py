0b7b60192d230213544d05ffd5f36fce95122ac8
cipher <ciphershadow1@gmail.com>
2025-09-29 15:52:18 +1000

    Removed blank line - line 13

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """function serializes Python object to JSON-formatted string"""
 
-
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
