6237ff537b0cb7a6cbe25c96074305a0e72df45c
CipherShadow <ciphershadow1@gmail.com>
2025-10-01 07:23:45 +1000

    Add 10-student.py task

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a class that defines a student by first_name, last_name
+and age attributes."""
+
+
+class Student:
+    """A class that defines a Student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns attribute names and values for Student object."""
+
+        if (
+            isinstance(attrs, list)
+            and all(isinstance(attrs, str) for attr in attrs)
+        ):
+            result = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    result[key] = self.__dict__[key]
+            return result
+        else:
+            return self.__dict__
