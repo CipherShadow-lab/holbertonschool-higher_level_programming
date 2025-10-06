64a0b438b244ac9a78946e987825b50dae93e53d
cipher <ciphershadow1@gmail.com>
2025-10-07 10:56:22 +1100

    "Add task_02_requests.py"

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(response.status_code)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(response.status_code)
+
+    if response.status_code == 200:
+        data = response.json()
+        filtered_posts = []
+
+        for post in data:
+            filtered_post = {
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }
+            filtered_posts.append(filtered_post)
+    
+    with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+        writer.writeheader()
+
+    for post in filtered_posts:
+        writer.writerow(post)
