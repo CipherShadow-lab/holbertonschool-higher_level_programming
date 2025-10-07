2a248c2a03a9cdb70ed53845af07f5410d0c9e80
cipher <ciphershadow1@gmail.com>
2025-10-07 15:24:38 +1100

    'fixed handling for failed request'

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -17,17 +17,19 @@ def fetch_and_save_posts():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {response.status_code}")
 
-    if response.status_code == 200:
-        data = response.json()
-        filtered_posts = []
+    if response.status_code != 200:
+        return
+    
+    data = response.json()
+    filtered_posts = []
 
-        for post in data:
-            filtered_post = {
-                "id": post["id"],
-                "title": post["title"],
-                "body": post["body"]
-            }
-            filtered_posts.append(filtered_post)
+    for post in data:
+        filtered_post = {
+            "id": post["id"],
+            "title": post["title"],
+            "body": post["body"]
+        }
+        filtered_posts.append(filtered_post)
     
     with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
@@ -35,5 +37,3 @@ def fetch_and_save_posts():
 
         for post in filtered_posts:
             writer.writerow(post)
-        else:
-            print(f"Status Code: {response.status_code}")
