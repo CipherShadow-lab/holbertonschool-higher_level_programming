da7229856e10c048b21dab18c3b91346bc3a7ff4
cipher <ciphershadow1@gmail.com>
2025-10-10 16:13:03 +1100

    "Add task_04_flask.py"

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+users = {}
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def status():
+    return jsonify({"status": "OK"})
+
+@app.route("/users/<username>")
+def get_user(username):
+    if username in users:
+        return jsonify(users[username])
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data["username"]
+
+    users[username] = {
+        "name": data["name"],
+        "age": data["age"],
+        "city": data["city"]
+    }
+
+    return jsonify({
+        "messge": "User added successfully",
+        "user": users[username]
+    }), 201
+
+if __name__ == "__main__":
+    app.run()
