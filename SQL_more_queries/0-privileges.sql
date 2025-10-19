3a9e748390b169498f86f8dd7d932ec7314e24b8
cipher <ciphershadow1@gmail.com>
2025-10-19 15:47:39 +1100

    fixed typo in localhost for user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Script lists all privileges of user_0d_1 and user_0d_2 on server (localhost)
 SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@hlocalhost;
+SHOW GRANTS FOR user_0d_2@localhost;
 
