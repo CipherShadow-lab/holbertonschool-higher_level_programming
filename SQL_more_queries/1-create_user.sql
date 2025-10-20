a37d1f091b1d25c1c2b4e02c51f416eead51d9c8
cipher <ciphershadow1@gmail.com>
2025-10-20 15:54:15 +1100

    fixed missing semi-colons

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Script creates user 'user_0d_1' on SQL server running on localhost
 -- Script will not fail if user already exists.
-DROP USER IF EXISTS 'user_01_1'@'localhost';
-CREATE USER 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-FLUSH PRIVILEGES
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 
