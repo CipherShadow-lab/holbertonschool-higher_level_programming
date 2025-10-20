08aa67ceeae3cc237741af00879310073e63d3e6
cipher <ciphershadow1@gmail.com>
2025-10-20 20:33:31 +1100

    corrected create table line to include db name

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@
 -- 'name' column is VARCHAR(256) and can't be NULL
 -- If database and table already exists, script will not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
