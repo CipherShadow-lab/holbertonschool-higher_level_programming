09a947d378aa1e2cf73b95a08a6889eb2f5de5a1
cipher <ciphershadow1@gmail.com>
2025-10-21 06:13:23 +1100

    add 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Script lists all cities of California found in database 'hbtn_0d_usa'.
+-- 'states' table contains one record where 'name' = 'California' (but id is different)
+-- Results are sorted in ascending order by 'cities.id'
+USE hbtn_0d_usa;
+SELECT id, name FROM cities WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+);
+
