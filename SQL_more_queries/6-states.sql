37f0d0bfc610336d483af81ca9a3d7ce94ed506b
cipher <ciphershadow1@gmail.com>
2025-10-20 20:36:11 +1100

    changed order of auto_increment and primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,7 +4,6 @@
 -- If database and table already exists, script will not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
-
