0dab0fbb7deb9df5a37f9b9cc29decdb3224a457
cipher <ciphershadow1@gmail.com>
2025-10-21 13:28:51 +1100

    add 14-my_genres.sql

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Script lists all genres of the show 'Dexter'
+-- 'tv_shows' contains only one record where 'title = Dexter'
+-- Results are sorted in ascending order
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
