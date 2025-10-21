9f16e2ed9b399a01f05e37ce21c597e1bd2b4cf5
cipher <ciphershadow1@gmail.com>
2025-10-21 11:57:45 +1100

    add tvshows_dump.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Script lists all shows in database 'hbtn_0d_tvshows' - that has at least one genre linked.
+-- Each record displays 'tv_shows.title' and 'tv_show_genres.genre_id'
+-- Results are sorted in ascending order by 'tv_shows.title' and 'tv_show_genres.genre_id'
+
