c6f19467c5cf48806bc15ea19804f2b12a1ff5b4
cipher <ciphershadow1@gmail.com>
2025-10-22 16:27:28 +1100

    add 8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script prints first 'State' object from database 'hbtn_0e_6_usa'."""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Access to database to get 'states' from database"""
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]),
+            pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).first()
+
+    if state is None:
+        print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
