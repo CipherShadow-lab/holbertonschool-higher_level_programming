ae445cce13aa22eec98f8611454b5d06a2743259
cipher <ciphershadow1@gmail.com>
2025-10-23 06:22:47 +1100

    add 10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script prints first 'State' object from database 'hbtn_0e_6_usa'."""
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Access to database to get 'states' from database"""
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]),
+            pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
