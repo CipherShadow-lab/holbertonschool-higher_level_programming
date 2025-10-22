17adf853359469510ee771a347fb7045de1651e7
cipher <ciphershadow1@gmail.com>
2025-10-22 15:25:13 +1100

    add model_state.py

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a State class mapped to the 'states' table."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create base class
+Base = declarative_base()
+
+class State():
+    """State class that maps to the 'states' table in MySQL."""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
