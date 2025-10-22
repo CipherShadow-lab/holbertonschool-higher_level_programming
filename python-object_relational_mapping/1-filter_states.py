049f3b0840c4dcf8963de445241e5a1d8320fbfd
cipher <ciphershadow1@gmail.com>
2025-10-22 13:52:55 +1100

    fixed doctstrings

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Script lists all states with a name starting with 'N' from database"""
+"""
+Script lists all states with a name starting with 'N'
+from database 'hbtn_0e_0_usa.
+"""
+
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
+
+    db = MySQLdb.connect(
             host="localhost",
             port=3306,
             user=argv[1],
             passwd=argv[2],
-            db=argv[3],
-            charset="utf8"
+            db=argv[3]
     )
 
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+cur = db.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
 rows = cur.fetchall()
 
 for row in rows:
