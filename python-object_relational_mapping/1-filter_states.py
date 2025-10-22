6ac9b84bc19dd0f9b3731532ae86b09299ee824f
cipher <ciphershadow1@gmail.com>
2025-10-22 13:37:16 +1100

    updated argvs

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,19 +2,20 @@
 """ Script lists all states with a name starting with N """
 
 import MySQLdb
+from sys import argv
 
 conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="root",
-        passwd="root",
-        db="hbtn_0e_0_usa",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
         charset="utf8"
 )
 
 cur = conn.cursor()
-cur.execute(SELECT * FROM states WHERE name LIKE 'N%')
-row = cur.fetchall()
+cur.execute(SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC)
+rows = cur.fetchall()
 
 for row in rows:
     print(row)
