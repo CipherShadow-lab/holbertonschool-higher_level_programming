59090ee83f7db66cea8804eb92c0e08ce22308fb
cipher <ciphershadow1@gmail.com>
2025-10-30 20:13:51 +1100

    add 7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const parsedValue = parseInt(arg);
+
+if (isNaN(parsedValue)) {
+  console.log('Missing number of occurrences');
+} else {
+  let output = '';
+  for (let i = 0; i < parsedValue; i++) {
+    output += 'C is fun\n';
+  }
+  console.log(output.trim());
+}
