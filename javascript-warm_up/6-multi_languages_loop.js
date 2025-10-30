3535c7637d0680c4dbcf8a94e2a24cf7033672f0
cipher <ciphershadow1@gmail.com>
2025-10-30 19:34:55 +1100

    updated and fixed code to remove newline at end

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(languages.join('\n'));
+let output = '';
+for (let i = 0; i < languages.length; i++) {
+  output += languages[i] + '\n';
+}
+console.log(output.trim());
