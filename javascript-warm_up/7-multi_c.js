668713ae734dc66e3bfe1b7ec0b7608cbd96b23f
cipher <ciphershadow1@gmail.com>
2025-10-30 20:17:24 +1100

    extended condition for arg value > 0

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const parsedValue = parseInt(arg);
 
 if (isNaN(parsedValue)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (parsedValue > 0) {
   let output = '';
   for (let i = 0; i < parsedValue; i++) {
     output += 'C is fun\n';
