09dd1bb949f63f49681e3766ea110fb5dc747e0b
cipher <ciphershadow1@gmail.com>
2025-10-30 14:36:04 +1100

    fixed extra newline at end of output

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,8 @@ const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let output = "";
 for (let i = 0; i < languages.length; i++) {
   output += languages[i] + '\n';
+  if (i < languages.length - 1) {
+    output += '\n';
+  }
 }
 console.log(output);
