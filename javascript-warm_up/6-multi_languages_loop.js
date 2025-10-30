a1252584d87c3977a7fb506bbe2de2038ded61bc
cipher <ciphershadow1@gmail.com>
2025-10-30 14:38:50 +1100

    removed non-permitted if statement

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let output = "";
-for (let i = 0; i < languages.length; i++) {
-  output += languages[i] + '\n';
-  if (i < languages.length - 1) {
-    output += '\n';
-  }
-}
-console.log(output);
+console.log(languages.join('\n'));
